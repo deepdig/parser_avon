92405371c74d627faeff7076f3a58f4b624a4de3
deepdig <deepdig@yandex.ru>
2017-05-30 17:51:28 +0300

    Parser_avon_v0.4_(загрузка всех необходимых каталогов и сортировка)

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
-    <title>Парсер zip каталога</title>
+    <title>Парсер zip каталога сайта Avon</title>
 </head>
-
 <body>
 
     <?php
-        $custom_main = "catalogs/custom/main/";
-        $custom_dop_1 = "catalogs/custom/dop_1/";
-        $custom_dop_2 = "catalogs/custom/dop_2/";
-        $custom_dop_3 = "catalogs/custom/dop_3/";
-        $catalog_path = "catalogs/other/";
+        set_time_limit(540);
+        $custom_main = "zip_catalog/custom/main/";
+        $custom_dop_1 = "zip_catalog/custom/dop_1/";
+        $custom_dop_2 = "zip_catalog/custom/dop_2/";
+        $custom_dop_3 = "zip_catalog/custom/dop_3/";
+        $catalog_path = "zip_catalog/other/";
 
         // качаем основную страницу в переменную $buf
         $buf=implode("",file("https://promoavon.ru/catalogs/"));
@@ -21,9 +20,9 @@
         // получем ссылки на страницы с каталогами в массив
         preg_match_all("/[hH]1>[ \r\n\t]*<[Aa][ \r\n\t]{1}[^>]*[Hh][Rr][Ee][Ff][^=]*=[ '\"\n\r\t]*([^ \"'>\r\n\t#]+)[^>]*>/",$buf,$url);
 
-          ///////////////////////////////////////////////////
-         ///////Получаем архивы с первого каталога//////////
-        ///////////////////////////////////////////////////
+        /////////////////////////////////////////////////////
+        /////// Получаем архивы с первого каталога //////////
+        /////////////////////////////////////////////////////
         $url_1=$url[1][0];
 
         // качаем первую страницу каталога в переменную $catalog_1
@@ -92,31 +91,261 @@
         $full_path_upload_1_1 = $full_path_upload_1_1."/".basename($file_zip_1_1);
         // Копируем файл
         if (copy($file_zip_1_1, $full_path_upload_1_1)){
-            echo "Каталог 1_1 успешно загружен на сервер<br>";
+            echo "Файл 1_1 успешно загружен в каталог ".$catalog_path ."<br>";
         }
 
         // Загрузка второго файла на сервер
         $full_path_upload_1_2 = $full_path_upload_1_2."/".basename($file_zip_1_2);
         // Копируем файл
         if (copy($file_zip_1_2, $full_path_upload_1_2)){
-            echo "Каталог 1_2 успешно загружен на сервер<br>";
+            echo "Файл 1_2 успешно загружен в каталог ".$catalog_path ."<br>";
         }
 
         // Загрузка третьего файла на сервер
         $full_path_upload_1_3 = $full_path_upload_1_3."/".basename($file_zip_1_3);
         // Копируем файл
         if (copy($file_zip_1_3, $full_path_upload_1_3)){
-            echo "Каталог 1_3 успешно загружен на сервер<br>";
+            echo "Файл 1_3 успешно загружен в каталог ".$catalog_path ."<br>";
         }
 
         // Загрузка четвертого файла на сервер
         $full_path_upload_1_4 = $full_path_upload_1_4."/".basename($file_zip_1_4);
         // Копируем файл
         if (copy($file_zip_1_4, $full_path_upload_1_4)){
-            echo "Каталог 1_4 успешно загружен на сервер<br>";
+            echo "Файл 1_4 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        ////////////////////////////////////////////////////
+        ////// Получаем архивы со второго каталога /////////
+        ////////////////////////////////////////////////////
+        $url_2=$url[1][1];
+
+        // качаем вторую страницу каталога в переменную $catalog_2
+        $catalog_2=implode("",file($url_2));
+
+        // получем ссылки на архивы zip с каталогами в массив
+        preg_match_all("/<[Aa][ \r\n\t]{1}[^>]*[Hh][Rr][Ee][Ff][^=]*=[ '\"\n\r\t]*([^ \"'>\r\n\t#]+[zZ][iI][pP])[^>]*>/",$catalog_2,$url_zip_2);
+
+        //ссылки на файлы первого каталога
+        $file_zip_2_1 = $url_zip_2[1][0];
+        $file_zip_2_2 = $url_zip_2[1][1];
+        $file_zip_2_3 = $url_zip_2[1][2];
+        $file_zip_2_4 = $url_zip_2[1][3];        
+
+        //загрузка в дирректорию other-----------------------
+        //создаем дирректорию для загрузки первого каталога
+        preg_match_all("/\d\d\d\d/", $file_zip_2_1, $catalog_2_dir_array);
+        $catalog_2_dir = $catalog_2_dir_array[0][0];
+        $uploaddir_2 = $catalog_path.$catalog_2_dir;
+        mkdir($uploaddir_2, 0755);
+
+        //создаем дирректории для загрузки файлов
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_2_1, $catalog_2_1_dir_array);
+        $catalog_2_1_file_dir = $catalog_2_1_dir_array[0][0];
+        $full_path_upload_2_1 = $uploaddir_2.$catalog_2_1_file_dir;
+        mkdir($full_path_upload_2_1, 0755);
+        
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_2_2, $catalog_2_2_dir_array);
+        $catalog_2_2_file_dir = $catalog_2_2_dir_array[0][1];
+        $full_path_upload_2_2 = $uploaddir_2.$catalog_2_2_file_dir;
+        mkdir($full_path_upload_2_2, 0755);
+
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_2_3, $catalog_2_3_dir_array);
+        $catalog_2_3_file_dir = $catalog_2_3_dir_array[0][0];
+        $full_path_upload_2_3 = $uploaddir_2.$catalog_2_3_file_dir;
+        mkdir($full_path_upload_2_3, 0755);
+
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_2_4, $catalog_2_4_dir_array);
+        $catalog_2_4_file_dir = $catalog_2_4_dir_array[0][0];
+        $full_path_upload_2_4 = $uploaddir_2.$catalog_2_4_file_dir;
+        mkdir($full_path_upload_2_4, 0755);
+
+        // Загрузка первого файла на сервер
+        $full_path_upload_2_1 = $full_path_upload_2_1."/".basename($file_zip_2_1);
+        // Копируем файл
+        if (copy($file_zip_2_1, $full_path_upload_2_1)){
+            echo "Файл 2_1 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        // Загрузка второго файла на сервер
+        $full_path_upload_2_2 = $full_path_upload_2_2."/".basename($file_zip_2_2);
+        // Копируем файл
+        if (copy($file_zip_2_2, $full_path_upload_2_2)){
+            echo "Файл 2_2 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        // Загрузка третьего файла на сервер
+        $full_path_upload_2_3 = $full_path_upload_2_3."/".basename($file_zip_2_3);
+        // Копируем файл
+        if (copy($file_zip_2_3, $full_path_upload_2_3)){
+            echo "Файл 2_3 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        // Загрузка четвертого файла на сервер
+        $full_path_upload_2_4 = $full_path_upload_2_4."/".basename($file_zip_2_4);
+        // Копируем файл
+        if (copy($file_zip_2_4, $full_path_upload_2_4)){
+            echo "Файл 2_4 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        ///////////////////////////////////////////////////
+        /////// Получаем архивы с третьего каталога ///////
+        ///////////////////////////////////////////////////
+        $url_3=$url[1][2];
+
+        // качаем третью страницу каталога в переменную $catalog_3
+        $catalog_3=implode("",file($url_3));
+
+        // получем ссылки на архивы zip с каталогами в массив
+        preg_match_all("/<[Aa][ \r\n\t]{1}[^>]*[Hh][Rr][Ee][Ff][^=]*=[ '\"\n\r\t]*([^ \"'>\r\n\t#]+[zZ][iI][pP])[^>]*>/",$catalog_3,$url_zip_3);
+
+        //ссылки на файлы первого каталога
+        $file_zip_3_1 = $url_zip_3[1][0];
+        $file_zip_3_2 = $url_zip_3[1][1];
+        $file_zip_3_3 = $url_zip_3[1][2];
+        $file_zip_3_4 = $url_zip_3[1][3];        
+
+        //загрузка в дирректорию other-----------------------
+        //создаем дирректорию для загрузки первого каталога
+        preg_match_all("/\d\d\d\d/", $file_zip_3_1, $catalog_3_dir_array);
+        $catalog_3_dir = $catalog_3_dir_array[0][0];
+        $uploaddir_3 = $catalog_path.$catalog_3_dir;
+        mkdir($uploaddir_3, 0755);
+
+        //создаем дирректории для загрузки файлов
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_3_1, $catalog_3_1_dir_array);
+        $catalog_3_1_file_dir = $catalog_3_1_dir_array[0][0];
+        $full_path_upload_3_1 = $uploaddir_3.$catalog_3_1_file_dir;
+        mkdir($full_path_upload_3_1, 0755);
+        
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_3_2, $catalog_3_2_dir_array);
+        $catalog_3_2_file_dir = $catalog_3_2_dir_array[0][1];
+        $full_path_upload_3_2 = $uploaddir_3.$catalog_3_2_file_dir;
+        mkdir($full_path_upload_3_2, 0755);
+
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_3_3, $catalog_3_3_dir_array);
+        $catalog_3_3_file_dir = $catalog_3_3_dir_array[0][0];
+        $full_path_upload_3_3 = $uploaddir_3.$catalog_3_3_file_dir;
+        mkdir($full_path_upload_3_3, 0755);
+
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_3_4, $catalog_3_4_dir_array);
+        $catalog_3_4_file_dir = $catalog_3_4_dir_array[0][0];
+        $full_path_upload_3_4 = $uploaddir_3.$catalog_3_4_file_dir;
+        mkdir($full_path_upload_3_4, 0755);
+
+        // Загрузка первого файла на сервер
+        $full_path_upload_3_1 = $full_path_upload_3_1."/".basename($file_zip_3_1);
+        // Копируем файл
+        if (copy($file_zip_3_1, $full_path_upload_3_1)){
+            echo "Файл 3_1 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        // Загрузка второго файла на сервер
+        $full_path_upload_3_2 = $full_path_upload_3_2."/".basename($file_zip_3_2);
+        // Копируем файл
+        if (copy($file_zip_3_2, $full_path_upload_3_2)){
+            echo "Файл 3_2 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        // Загрузка третьего файла на сервер
+        $full_path_upload_3_3 = $full_path_upload_3_3."/".basename($file_zip_3_3);
+        // Копируем файл
+        if (copy($file_zip_3_3, $full_path_upload_3_3)){
+            echo "Файл 3_3 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        // Загрузка четвертого файла на сервер
+        $full_path_upload_3_4 = $full_path_upload_3_4."/".basename($file_zip_3_4);
+        // Копируем файл
+        if (copy($file_zip_3_4, $full_path_upload_3_4)){
+            echo "Файл 3_4 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+
+        ///////////////////////////////////////////////////
+        ////// Получаем архивы с четвертого каталога //////
+        ///////////////////////////////////////////////////
+        $url_4=$url[1][3];
+
+        // качаем четвертую страницу каталога в переменную $catalog_4
+        $catalog_4=implode("",file($url_4));
+
+        // получем ссылки на архивы zip с каталогами в массив
+        preg_match_all("/<[Aa][ \r\n\t]{1}[^>]*[Hh][Rr][Ee][Ff][^=]*=[ '\"\n\r\t]*([^ \"'>\r\n\t#]+[zZ][iI][pP])[^>]*>/",$catalog_4,$url_zip_4);
+
+        //ссылки на файлы первого каталога
+        $file_zip_4_1 = $url_zip_4[1][0];
+        $file_zip_4_2 = $url_zip_4[1][1];
+        $file_zip_4_3 = $url_zip_4[1][2];
+        $file_zip_4_4 = $url_zip_4[1][3];        
+
+        //загрузка в дирректорию other-----------------------
+        //создаем дирректорию для загрузки первого каталога
+        preg_match_all("/\d\d\d\d/", $file_zip_4_1, $catalog_4_dir_array);
+        $catalog_4_dir = $catalog_4_dir_array[0][0];
+        $uploaddir_4 = $catalog_path.$catalog_4_dir;
+        mkdir($uploaddir_4, 0755);
+
+        //создаем дирректории для загрузки файлов
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_4_1, $catalog_4_1_dir_array);
+        $catalog_4_1_file_dir = $catalog_4_1_dir_array[0][0];
+        $full_path_upload_4_1 = $uploaddir_4.$catalog_4_1_file_dir;
+        mkdir($full_path_upload_4_1, 0755);
+        
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_4_2, $catalog_4_2_dir_array);
+        $catalog_4_2_file_dir = $catalog_4_2_dir_array[0][1];
+        $full_path_upload_4_2 = $uploaddir_4.$catalog_4_2_file_dir;
+        mkdir($full_path_upload_4_2, 0755);
+
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_4_3, $catalog_4_3_dir_array);
+        $catalog_4_3_file_dir = $catalog_4_3_dir_array[0][0];
+        $full_path_upload_4_3 = $uploaddir_4.$catalog_4_3_file_dir;
+        mkdir($full_path_upload_4_3, 0755);
+
+        preg_match_all("/\/\w*\d\d\d\d/", $file_zip_4_4, $catalog_4_4_dir_array);
+        $catalog_4_4_file_dir = $catalog_4_4_dir_array[0][0];
+        $full_path_upload_4_4 = $uploaddir_4.$catalog_4_4_file_dir;
+        mkdir($full_path_upload_4_4, 0755);
+
+        // Загрузка первого файла на сервер
+        $full_path_upload_4_1 = $full_path_upload_4_1."/".basename($file_zip_4_1);
+        // Копируем файл
+        if (copy($file_zip_4_1, $full_path_upload_4_1)){
+            echo "Файл 4_1 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+        else {
+            echo "Файл 4_1 отсутствует на сайте источнике <br>";   
+        }
+
+        // Загрузка второго файла на сервер
+        $full_path_upload_4_2 = $full_path_upload_4_2."/".basename($file_zip_4_2);
+        // Копируем файл
+        if (copy($file_zip_4_2, $full_path_upload_4_2)){
+            echo "Файл 4_2 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+        else {
+            echo "Файл 4_2 отсутствует на сайте источнике <br>";   
+        }
+
+        // Загрузка третьего файла на сервер
+        $full_path_upload_4_3 = $full_path_upload_4_3."/".basename($file_zip_4_3);
+        // Копируем файл
+        if (copy($file_zip_4_3, $full_path_upload_4_3)){
+            echo "Файл 4_3 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+        else {
+            echo "Файл 4_3 отсутствует на сайте источнике <br>";   
+        }
+
+        // Загрузка четвертого файла на сервер
+        $full_path_upload_4_4 = $full_path_upload_4_4."/".basename($file_zip_4_4);
+        // Копируем файл
+        if (copy($file_zip_4_4, $full_path_upload_4_4)){
+            echo "Файл 4_4 успешно загружен в каталог ".$catalog_path ."<br>";
+        }
+        else {
+            echo "Файл 4_4 отсутствует на сайте источнике <br>";   
         }
     ?>
 
 </body>
-
 </html>
