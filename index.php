e56e1fd0e869b9c820f24827bb36900d6bb1d388
deepdig <deepdig@yandex.ru>
2017-05-30 15:34:38 +0300

    Парсе_zip_v0.2 (с распределением файлов по дирректориям)

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
 <body>
 
     <?php
-
-        $catalog_path = "catalogs/";
+        $custom_main = "catalogs/custom/main/";
+        $custom_dop_1 = "catalogs/custom/dop_1/";
+        $custom_dop_2 = "catalogs/custom/dop_2/";
+        $custom_dop_3 = "catalogs/custom/dop_3/";
+        $catalog_path = "catalogs/other/";
 
         // качаем основную страницу в переменную $buf
         $buf=implode("",file("https://promoavon.ru/catalogs/"));
@@ -35,6 +38,29 @@
         $file_zip_1_3 = $url_zip_1[1][2];
         $file_zip_1_4 = $url_zip_1[1][3];
 
+        //загрузка в текущий каталог custom-----------------------               
+        // Копируем 1 файл в основной каталог
+        $custom_main = $custom_main."/".basename($file_zip_1_1);
+        if (copy($file_zip_1_1, $custom_main)){
+            echo "Файл 1_1 успешно загружен в текущий каталог <br>";
+        }
+        // Копируем 2 файл в дополнительный каталог
+        $custom_dop_1 = $custom_dop_1."/".basename($file_zip_1_2);
+        if (copy($file_zip_1_2, $custom_dop_1)){
+            echo "Файл 1_2 успешно загружен в текущий каталог <br>";
+        }
+        // Копируем 3 файл в дополнительный каталог
+        $custom_dop_2 = $custom_dop_2."/".basename($file_zip_1_3);
+        if (copy($file_zip_1_3, $custom_dop_2)){
+            echo "Файл 1_3 успешно загружен в текущий каталог <br>";
+        }
+        // Копируем 4 файл в дополнительный каталог
+        $custom_dop_3 = $custom_dop_3."/".basename($file_zip_1_4);
+        if (copy($file_zip_1_4, $custom_dop_3)){
+            echo "Файл 1_4 успешно загружен в текущий каталог <br>";
+        }
+
+        //загрузка в дирректорию other-----------------------
         //создаем дирректорию для загрузки первого каталога
         preg_match_all("/\d\d\d\d/", $file_zip_1_1, $catalog_1_dir_array);
         $catalog_1_dir = $catalog_1_dir_array[0][0];
